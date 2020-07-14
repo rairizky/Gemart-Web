3fe66eb0ac4d99faa2d8212c1ae9d293c4dc4627
rairizky <32klolita@gmail.com>
2020-07-14 21:23:23 +0700

    api permission-secure login role url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,16 @@ class UsersController < ApplicationController
 
     before_action :authorized, only: [:auto_login]
 
-    def register_gerai
-        @user = User.create(user_params_gerai)
-        if @user.valid?
-            token = encode_token({ user_id: @user.id })
-            render json: { status: 0, token: token, user: @user }
-        else
-            render json: { status: 1, error: @user.errors.full_messages }
-        end
-    end
-
-    def login_gerai
+    def login_admin
         @user = User.find_by(email: params[:email])
         if @user 
             if @user.authenticate(params[:password])
                 token = encode_token({ user_id: @user.id })
-                render json: { status: 0, token: token, user: @user }
+                if @user.role_id != 1
+                    render json: { status: 1, error: "You are not allowed to login in this page!" }
+                else
+                    render json: { status: 0, token: token, user: @user }
+                end
             else 
                 render json: { status: 1, error: 'Email and Password not match' }
             end
@@ -41,7 +35,39 @@ class UsersController < ApplicationController
         if @user 
             if @user.authenticate(params[:password])
                 token = encode_token({ user_id: @user.id })
-                render json: { status: 0, token: token, user: @user }
+                if @user.role_id != 2
+                    render json: { status: 1, error: "You are not allowed to login in this page!" }
+                else
+                    render json: { status: 0, token: token, user: @user }
+                end
+            else 
+                render json: { status: 1, error: 'Email and Password not match' }
+            end
+        else
+            render json: { status: 1, error: 'Email not found' }
+        end
+    end
+
+    def register_gerai
+        @user = User.create(user_params_gerai)
+        if @user.valid?
+            token = encode_token({ user_id: @user.id })
+            render json: { status: 0, token: token, user: @user }
+        else
+            render json: { status: 1, error: @user.errors.full_messages }
+        end
+    end
+
+    def login_gerai
+        @user = User.find_by(email: params[:email])
+        if @user 
+            if @user.authenticate(params[:password])
+                token = encode_token({ user_id: @user.id })
+                if @user.role_id != 3
+                    render json: { status: 1, error: "You are not allowed to login in this page!" }
+                else
+                    render json: { status: 0, token: token, user: @user }
+                end
             else 
                 render json: { status: 1, error: 'Email and Password not match' }
             end
@@ -55,13 +81,13 @@ class UsersController < ApplicationController
     end
 
     private
-    def user_params_gerai
-        role = { role_id: 3 }
+    def user_params_supplier
+        role = { role_id: 2 }
         params.permit(:username, :email, :password).merge(role)
     end
 
-    def user_params_supplier
-        role = { role_id: 2 }
+    def user_params_gerai
+        role = { role_id: 3 }
         params.permit(:username, :email, :password).merge(role)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
+  # admin
+  post '/api/v1/login_admin', to: 'users#login_admin'
+
+  # gerai
   post '/api/v1/register_gerai', to: 'users#register_gerai'
   post '/api/v1/login_gerai', to: 'users#login_gerai'
 
+  # supplier
   post '/api/v1/register_supplier', to: 'users#register_supplier'
   post '/api/v1/login_supplier', to: 'users#login_supplier'
 
+  # home
   get '/api/v1/auto_login', to: 'users#auto_login'
+
 end
